884a3990ee833904e69be7865711c8d9fd85f08c
Dejan Eric <dejan.eric@outlook.com>
2025-02-27 11:51:18 +0100

    Change Footer logic plus delete the headings

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -6,22 +6,54 @@ function decorateFooterTop(block) {
   const footerTop = block.querySelector('.footer-top');
   const tempDiv = footerTop.querySelector('.section-container>div');
   const children = [...footerTop.querySelector('.section-container>div').children];
+
   let index = 0;
+  let topIndex = 0;
   tempDiv.innerHTML = '';
 
   while (index < children.length) {
-    const topItem = document.createElement('div');
-    topItem.classList.add('footer-top-item');
-    topItem.appendChild(children[index]);
+    let topItem;
+    if (topIndex < 5) {
+      topItem = document.createElement('div');
+      topItem.classList.add('footer-top-item');
+      topIndex += 1;
+    } else {
+      topItem = tempDiv.querySelector('.footer-top-item:last-child');
+    }
+
+    if (children[index].tagName === 'H5') {
+      const a = children[index].querySelector('a');
+      if (a) {
+        a.classList.add('h5-style');
+        topItem.appendChild(a);
+      }
+    } else {
+      topItem.appendChild(children[index]);
+    }
     index += 1;
 
     while (index < children.length) {
+      if (topIndex > 5) {
+        topItem.appendChild(children[index]);
+        index += 1;
+        break;
+      }
       if (children[index].tagName === 'H5') {
         if (!children[index + 1] || (children[index - 1].tagName === 'H5' && children[index + 1].tagName !== 'UL')) {
-          topItem.appendChild(children[index]);
+          const a = children[index].querySelector('a');
+          if (a) {
+            a.classList.add('h5-style');
+            topItem.appendChild(a);
+          }
         } else {
           break;
         }
+      } else if (children[index].tagName === 'H6') {
+        const h6 = children[index];
+        const p = document.createElement('p');
+        p.classList.add('h6-style');
+        p.innerHTML = h6.innerHTML;
+        topItem.appendChild(p);
       } else {
         topItem.appendChild(children[index]);
       }
@@ -49,7 +81,7 @@ export default async function decorate(block) {
   const footerPath = footerMeta || (getLanguage() === 'en' ? '/footer' : `/${getLanguage()}/footer`);
   const resp = await fetch(
     `${footerPath}.plain.html`,
-    window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {}
+    window.location.pathname.endsWith('/footer') ? { cache: 'reload' } : {},
   );
 
   if (resp.ok) {
