ad39dc626cd29ed4e34ebc9eafed143a4974c99c
Dejan Eric <dejan.eric@outlook.com>
2025-08-14 16:13:48 +0200

    Upadated hlx. with aem.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -759,7 +759,7 @@ export function getEnvType(hostname = window.location.hostname) {
     'main--sunstar--sunstar-global.hlx.live': 'live',
     'main--sunstar--sunstar-global.aem.page': 'preview',
     'main--sunstar--sunstar-global.aem.live': 'live',
-    'ga-integration--sunstar--sunstar-global.hlx.live': 'live',
+    'ga-integration--sunstar--sunstar-global.aem.live': 'live',
   };
   return fqdnToEnvType[hostname] || 'dev';
 }
