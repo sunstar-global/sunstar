b125765ab4d952d0e205d50488564faf313389b1
Dejan Eric <dejan.eric@outlook.com>
2026-04-07 14:20:31 +0200

    feat: enhance bar chart generation with configurable titles and improved data handling

--- blocks/charts/charts.js
+++ blocks/charts/charts.js
@@ -1,33 +1,100 @@
 /* eslint-disable no-unused-vars */
 
 import { loadChartJs } from '../../scripts/scripts.js';
+import { readBlockConfig } from '../../scripts/lib-franklin.js';
 
-function generateBarChart(labels, data, block) {
-  // append script inside head to load chart.js
-
-  // create canvas element
+function generateBarChart(labels, data, config, block) {
   const canvas = document.createElement('canvas');
   canvas.ctx = canvas.getContext('2d');
   block.appendChild(canvas);
+  let title = {};
+  if (config.title) {
+    title = {
+      display: true,
+      text: config.title,
+      position: 'top',
+      align: 'start',
+      color: '#00587c',
+      font: {
+        size: 24,
+        weight: 'normal',
+      },
+      padding: {
+        top: 10,
+        bottom: 30,
+      },
+    };
+  }
+
+  // chck what is the max value in data and set stepSize accordingly to have around 6 steps in y axis + 10% buffer on top
+  const stepSize = Math.ceil(Math.max(...data) / 6) + Math.ceil(Math.max(...data) / 10);
+
+  // set the last bar to be thicker if it has the max value, otherwise all bars will have the same thickness
+  const barTicknessArray = data.map((value) => (value === Math.max(...data) ? 60 : 90));
+
+  const lastIndex = data.length - 1;
+  const mainData = data.map((v, i) => (i === lastIndex ? null : v));
+  const mainLabels = labels.map((v, i) => (i === lastIndex ? '' : v));
+  const lastBarData = data.map((v, i) => (i === lastIndex ? v : null));
+  const lastBarLabel = labels.map((v, i) => (i === lastIndex ? v : ''));
 
   const ctx = canvas;
-  // eslint-disable-next-line no-undef
-  const mychart = new Chart(ctx, {
+
+  // eslint-disable-next-line no-undef, no-new
+  const chart = new Chart(ctx, {
     type: 'bar',
+    title: 'Chart Title',
+    aspectRatio: 1,
     data: {
+      stacked: true,
       labels,
       datasets: [
         {
-          data,
-          borderWidth: 1,
-          backgroundColor: ['#4D89A3', '#00577C', '#F28B2D'],
+          data: mainData,
+          borderWidth: 0,
+          borderRadius: 16,
+          backgroundColor: ['#4D89A3', '#00577C'],
+          barThickness: 60,
+          grouped: false,
+        },
+        {
+          data: lastBarData,
+          borderWidth: 0,
+          borderRadius: 16,
+          backgroundColor: '#F28B2D',
+          barThickness: 90,
+          grouped: false,
         },
       ],
     },
     options: {
+      plugins: {
+        title: { ...title },
+        legend: {
+          display: false,
+        },
+      },
       scales: {
         y: {
           beginAtZero: true,
+          ticks: {
+            color: '#00587c',
+            stepSize,
+          },
+          border: {
+            width: 0,
+          },
+        },
+        x: {
+          ticks: {
+            color: '#00587c',
+          },
+          border: {
+            width: 0,
+          },
+          grid: {
+            display: false,
+          },
         },
       },
     },
@@ -69,8 +136,12 @@ function getDataFromBlock(block) {
 
 export default async function decorate(block) {
   await loadChartJs();
+  const blockCfg = readBlockConfig(block);
+
+  console.log(blockCfg);
+
   const { labels, data } = getDataFromBlock(block);
-  generateBarChart(labels, data, block);
+  generateBarChart(labels, data, blockCfg, block);
   // remove everything except canvas element from block
   Array.from(block.children).forEach((child) => {
     if (child.tagName.toLowerCase() !== 'canvas') {
